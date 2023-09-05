30399b0795c841b61cfd4d3889dad3e2047c9c57
Robert Olejnik <shaman44@gmail.com>
2023-09-05 19:22:44 +0200

    add after files

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -34,8 +34,7 @@ return require('packer').startup(function(use)
       'kyazdani42/nvim-tree.lua',
       requires = {
         'kyazdani42/nvim-web-devicons', -- optional, for file icon
-      },
-      config = function() require'nvim-tree'.setup {} end
+      }
   }
 
   use {
